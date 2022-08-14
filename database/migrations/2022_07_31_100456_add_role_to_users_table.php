e08e68c3f7b777623cfaad8082c1a4bc28f4d4fd
Raimondas Palaguta <r.palaguta@gmail.com>
2022-08-15 00:49:21 +0300

    added endpoint to AuthController

--- database/migrations/2022_07_31_100456_add_role_to_users_table.php
+++ database/migrations/2022_07_31_100456_add_role_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')->after('id');
+            $table->unsignedBigInteger('role_id')->after('id')->default(2);
             $table->foreign('role_id')->references('id')->on('user_roles');
         });
     }
